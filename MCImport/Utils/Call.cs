1ac73bf1b05f1605ec354871fd75e2bdc934d972
Domenico Tenace <domenicotenace99@gmail.com>
2023-04-26 14:15:34 +0200

    feat: create call utils

---
+++ MCImport/Utils/Call.cs
@@ -0,0 +1,42 @@
+﻿using MCImport.Models;
+using RestSharp;
+
+namespace MCImport.Utils
+{
+    internal class Call
+    {
+        public static async Task<RestResponse> GetFromService(string url)
+        {
+            try
+            {
+                using RestClient client = new();
+
+
+                RestRequest request = new(url, Method.Get);
+
+                RestResponse response = await client.GetAsync(request);
+
+                return response;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static async void Post(Product product)
+        {
+            try
+            {
+                using McproductsContext database = new();
+
+                await database.Products.AddAsync(product);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
