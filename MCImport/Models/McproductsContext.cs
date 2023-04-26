a5cfe9d3a15570aeac31676d4b730f7f5dd3e6f6
Domenico Tenace <domenicotenace99@gmail.com>
2023-04-26 12:43:39 +0200

    feat: scaffold models from db

---
+++ MCImport/Models/McproductsContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MCImport.Models;
+
+public partial class McproductsContext : DbContext
+{
+    public McproductsContext()
+    {
+    }
+
+    public McproductsContext(DbContextOptions<McproductsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-8IV98MO\\SQLEXPRESS03;Database=MCProducts;Trusted_Connection=true;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Price).HasColumnType("decimal(9, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MCImport/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MCImport.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string? Description { get; set; }
+
+    public string Category { get; set; } = null!;
+
+    public string? Image { get; set; }
+
+    public int? Quantity { get; set; }
+}
